514085f92cd9478d0b39ffb0a4939eaeffdc1241
khoazero123 <khoazero123@gmail.com>
2019-12-20 09:27:26 +0700

    a

---
+++ precache-manifest.71030b49a418d512e0a58a46cf77a4d8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bbcc4c9040529176f6d83fcc7248ea9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "13a081f84c9bba43b372",
+    "url": "./static/css/2.bea6f0b1.chunk.css"
+  },
+  {
+    "revision": "d4171a7cb3e25ffa6dfb",
+    "url": "./static/css/main.ec1a3a99.chunk.css"
+  },
+  {
+    "revision": "13a081f84c9bba43b372",
+    "url": "./static/js/2.22b0989b.chunk.js"
+  },
+  {
+    "revision": "872a74aa0d69d7a3c6eb5a814129c44b",
+    "url": "./static/js/2.22b0989b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d4171a7cb3e25ffa6dfb",
+    "url": "./static/js/main.4b1d19d8.chunk.js"
+  },
+  {
+    "revision": "8b85018932a330f2e6da",
+    "url": "./static/js/runtime-main.7ec3fc4a.js"
+  }
+]);
